69e693f74584c8ca5c3533293286048b4da4af2d
NewWeiNern <44150474+NewWeiNern@users.noreply.github.com>
2019-05-29 11:46:44 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,10 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js'),
+      request = require("request");
 const client = new Discord.Client();
 let current_id;
 client.on('ready', () => {
     console.log("Bot is ready for deploy!");
+    request("https://cors-anywhere.herokuapp.com/"+"https://www.rockstargames.com/newswire/tags.json?tags=702&page=1");
     });
 
 client.on('message', message => {

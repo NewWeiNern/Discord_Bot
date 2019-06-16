17f8d1eef751a88e194e1d2cca299e50dcc7d307
NewWeiNern <44150474+NewWeiNern@users.noreply.github.com>
2019-06-17 07:33:17 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,35 @@
 const Discord = require('discord.js'),
       XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const client = new Discord.Client();
-let current_id;
-client.on('ready', () => {
-      console.log("Bot is ready for deploy!");
-      var xml = new XMLHttpRequest();
-      xml.open("GET","https://www.rockstargames.com/newswire/tags.json?tags=702&page=1");
-      xml.send();
-      xml.onreadystatechange = function(e){
-            console.log(e);
-            if(this.status == 200 && this.readyState == 4){
-                  console.log(e);
-            }
-      }
-    });
+const affect_channel = "gta-news";
+
+function doCORSRequest(options, result){
+    let cors = "https://cors-anywhere.herokuapp.com/";
+    var xml = new XMLHttpRequest();
+    xml.open(options.method, cors + options.url);
+    xml.setRequestHeader("x-requested-with", "XMLHttpRequest");
+
+    xml.onreadystatechange = function(e){
+        if(this.readyState == 4 && this.status == 200 && this.responseText.length > 1)
+            result(this.responseText);
+    }
+    xml.send();
+}
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+    const regex = /<p.+><img.+|<div.+|<\/?.+?>/gm;
+
+    if(message.content == "news" && affect_channel == message.channel.name){
+        doCORSRequest({method:"GET", url:"https://www.rockstargames.com/newswire/tags.json?tags=702&page=1"}, e=>{
+            var id = JSON.parse(e).posts[0].id;
+            doCORSRequest({method:"GET", url:"https://www.rockstargames.com/newswire/get-article/" + id + ".json?autoplay=false"}, e=>{
+                message.channel.send(JSON.parse(e).article.content.replace(regex, ""));
+            });
+
+
+        });
+    }
+
 });
 
 // THIS  MUST  BE  THIS  WAY
